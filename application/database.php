fed35ea79739ac949b295066c6f054c09de2169c
sshpark <huangjiaming@gmail.com>
2017-11-21 21:04:09 +0800

    update database.php

--- application/database.php
+++ application/database.php
@@ -15,11 +15,11 @@ return [
     // 服务器地址
     'hostname'       => '127.0.0.1',
     // 数据库名
-    'database'       => 'phpsys',
+    'database'       => '',
     // 用户名
-    'username'       => 'root',
+    'username'       => '',
     // 密码
-    'password'       => '715729',
+    'password'       => '',
     // 端口
     'hostport'       => '3306',
     // 连接dsn
