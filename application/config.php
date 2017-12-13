de284822375cafd9ee95e28af1d35dc7b0b65e1a
sshpark <huangjiaming@gmail.com>
2017-12-13 23:28:22 +0800

    添加了__ROOT__变量

--- application/config.php
+++ application/config.php
@@ -132,7 +132,8 @@ return [
 
     // 视图输出字符串内容替换
     'view_replace_str'       => [
-        '__STATIC__' => '/static',
+        '__STATIC__' => '/PHP-Sys/public/static',
+        '__ROOT__' => '/PHP-Sys/public/',
     ],
     // 默认跳转页面对应的模板文件
     'dispatch_success_tmpl'  => THINK_PATH . 'tpl' . DS . 'dispatch_jump.tpl',
